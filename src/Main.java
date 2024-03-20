14569bd21cbdc77c07aa93186aec594d479de707
Christofer Bryan <christoferbryan45@gmail.com>
2024-03-20 16:07:55 +0800

    feat(main): add an array of numbers and a for loop

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("This is a Java file for Git practice.");
+
+        int[] myNum = {10, 20, 30, 40};
+
+        for(int num : myNum){
+            System.out.println(num);
+        }
+    }
+}
